e709bc48f2a2338ae850927dd5dbb53f28f97cc2
SovaSofya <113455639+SovaSofya@users.noreply.github.com>
2023-05-16 16:35:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,16 @@
-import os
-
-import pandas as pd
-
 import streamlit as st
-import streamlit.components.v1 as components
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+st.write("# Real Time Sentiment Analysis")
 
-# Create a _RELEASE constant. We'll set this to False while we're developing
-# the component, and True when we're ready to package and distribute it.
-_RELEASE = False
+user_input = st.text_input("Please rate our services »: ")
+nltk.download("vader_lexicon")
+s = SentimentIntensityAnalyzer()
+score = s.polarity_scores(user_input)
 
-if not _RELEASE:
-    _selectable_data_table = components.declare_component(
-        "selectable_data_table",
-        url="http://localhost:3001",
-    )
-else:
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "frontend/build")
-    _selectable_data_table = components.declare_component("selectable_data_table", path=build_dir)
-
-
-def selectable_data_table(data, key=None):
-    return _selectable_data_table(data=data, default=[], key=key)
-
-# Test code to play with the component while it's in development.
-# During development, we can run this just as we would any other Streamlit
-# app: `$ streamlit run selectable_data_table/__init__.py`
-if not _RELEASE:
-    raw_data = {
-        "First Name": ["Jason", "Molly", "Tina", "Jake", "Amy"],
-        "Last Name": ["Miller", "Jacobson", "Ali", "Milner", "Smith"],
-        "Age": [42, 52, 36, 24, 73],
-    }
-    df = pd.DataFrame(raw_data, columns=["First Name", "Last Name", "Age"])
-
-    rows = selectable_data_table(df)
-    if rows:
-        st.write("You have selected", rows)
+if score == 0:
+st.write(" ")
+elif score["neg"] != 0:
+st.write("# Negative")
+elif score["pos"] != 0:
+st.write("# Positive")
